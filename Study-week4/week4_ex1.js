1d2fbeb59c2b82e6c85a2095a7c98d8a299cd9d5
JJONGHYUNI <whdgusdl8792@gmail.com>
2022-03-27 00:51:38 +0900

    Add Text File [ week4_ex1.html]

---
+++ Study-week4/week4_ex1.js
@@ -0,0 +1,9 @@
+function pageLoad(){
+	let btn= document.getElementById("myBtn");
+	btn.onclick = displayDate;
+}
+
+function displayDate(){
+	document.getElementById("demo").innerHTML = Date();
+}
+window.onload = pageLoad;
